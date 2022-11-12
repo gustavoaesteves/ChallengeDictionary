d1537a2d7b26ea66678a16c486da7841f3996d47
gustavoaesteves <gustavoalves19992@gmail.com>
2022-11-12 11:11:11 -0300

    Install Angular Material

--- ChallengeDictionary/src/app/app.module.ts
+++ ChallengeDictionary/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginPageComponent } from './page/Login/login.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { LoginPageComponent } from './page/Login/login.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
