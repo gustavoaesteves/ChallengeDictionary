7f7e8ae1ea9da49752a99d848d122d24bbfd4502
gustavoaesteves <gustavoalves19992@gmail.com>
2022-11-12 11:08:36 -0300

    Create page and route Login

--- ChallengeDictionary/src/app/app-routing.module.ts
+++ ChallengeDictionary/src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginPageComponent } from './page/Login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'login',},
+  { path: 'login', component: LoginPageComponent },
+  /*{ path: 'Irep', component: HomeComponent,
+    children: [
+      { path: 'dashboard', component: AdvertsPageComponent },
+      { path: 'announce', component: AnnouncePageComponent },
+      { path: 'favorites', component: FavoritesPageComponent },
+      { path: 'description/:advert', component: DescriptionPageComponent },
+    ]
+  },*/
+  { path: '**', redirectTo: 'login',},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ ChallengeDictionary/src/app/page/Login/login.component.ts
@@ -0,0 +1,10 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'login-page',
+  templateUrl: './login.component.html',
+})
+export class LoginPageComponent {
+
+  constructor() { }
+}
